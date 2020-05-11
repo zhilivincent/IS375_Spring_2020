3a8c52053c4948c86f570d524a5a3a3d5adc96df
zhilivincent <63615186+zhilivincent@users.noreply.github.com>
2020-05-11 13:25:16 -0700

    Add files via upload

---
+++ ON/Module5/Encapsulation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class DemoEncap
+    {
+
+        // private variables declared
+        // these can only be accessed by
+        // public methods of class
+        private String studentName;
+        private int studentAge;
+
+        // using accessors to get and
+        // set the value of studentName
+        public String Name
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+            }
+        }
+
+        // using accessors to get and
+        // set the value of studentAge
+        public int Age
+        {
+            get
+            {
+                return studentAge;
+            }
+            set
+            {
+                studentAge = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating object
+            DemoEncap obj = new DemoEncap();
+
+            // calls set accessor of the property Name,
+            // and pass "Ankita" as value of the
+            // standard field 'value'
+            obj.Name = "Ankita";
+
+            // call set accessor of the property Age,
+            // and pass "21" as value of the
+            // standard field 'value'
+            obj.Age = 21;
+
+            // Displaying values of the variables
+            Console.WriteLine("Name: " + obj.Name);
+            Console.WriteLine("Age: " + obj.Age);
+        }
+    }
+}
